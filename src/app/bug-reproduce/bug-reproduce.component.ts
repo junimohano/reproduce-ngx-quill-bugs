0498121e69adf6f37a61e2e9f88c4789eaeeee57
Jun An <jun.an@nymity.com>
2019-03-19 11:27:34 -0400

    removed

--- src/app/bug-reproduce/bug-reproduce.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-bug-reproduce',
-  templateUrl: './bug-reproduce.component.html'
-})
-export class BugReproduceComponent {
-  value = `<div>test</div><ul><li>1</li><li>2</li><li>3</li><li>4</li></ul><div><br></div>`;
-}
--- src/app/format-html/format-html.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'app-format-html',
-  templateUrl: './format-html.component.html'
-})
-export class FormatHtmlComponent {
-  htmlFormat = '<ul><li>1</li><li>2</li><li>3</li></ul>'
-}
