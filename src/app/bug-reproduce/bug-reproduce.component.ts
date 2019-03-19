ccd14fef7ccd76c867d854dbf38efbe5fb8c4d1c
Jun An <jun.an@nymity.com>
2019-03-19 11:18:19 -0400

    reproducable code pushed

---
+++ src/app/bug-reproduce/bug-reproduce.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bug-reproduce',
+  templateUrl: './bug-reproduce.component.html'
+})
+export class BugReproduceComponent {
+  value = `<div>test</div><ul><li>1</li><li>2</li><li>3</li><li>4</li></ul><div><br></div>`;
+}
---
+++ src/app/format-text/format-text.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-format-text',
+  templateUrl: './format-text.component.html'
+})
+export class FormatTextComponent {
+  textFormat = 'Hello World!'
+}
